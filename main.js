6c0dd6c408a68fc1908f3a07fe2efdc89f231907
BuildTools <unconfigured@null.spigotmc.org>
2025-03-11 20:04:13 +0100

    added map

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-var map = L.map('map', {
-    center: [51.505, -0.09],
-    zoom: 13
-});
+var map = L.map('map').setView([50.6751568996643, 7.161392967579368], 14);
+        mapLink = 
+            '<a href="http://openstreetmap.org">OpenStreetMap</a>';
+        L.tileLayer(
+            'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; ' + mapLink + ' Contributors',
+            maxZoom: 18,
+            }).addTo(map);
