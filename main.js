8a305b09862da157bbbd592d4c82145b3746000d
BuildTools <unconfigured@null.spigotmc.org>
2025-03-13 20:00:48 +0100

    backend and design

--- main.js
+++ main.js
@@ -6,3 +6,37 @@ var map = L.map('map').setView([50.6751568996643, 7.161392967579368], 14);
             attribution: '&copy; ' + mapLink + ' Contributors',
             maxZoom: 18,
             }).addTo(map);
+
+var point = 0;
+
+function button_press() {
+    if (point == 0){
+        XML = new XMLHttpRequest();
+        XML.open("GET", "http://localhost:5000/getQuestion", true);
+        XML.onreadystatechange = function() {
+            if (XML.readyState == 4 && XML.status == 200) {
+                document.getElementById("text").innerHTML = XML.responseText;
+            }
+            point = 1;
+        };
+        XML.send();
+    } else if (point == 1){
+        XML = new XMLHttpRequest();
+        XML.open("POST", "http://localhost:5000/compareResult", true);
+        XML.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        params = {"guess": document.getElementById("input").value};
+
+
+        XML.onreadystatechange = function() {
+            if (XML.readyState == 4 && XML.status == 200) {
+                if (XML.responseText == "True"){
+                    document.getElementById("text").innerHTML = "Correct!";
+                }
+                else {
+                    document.getElementById("text").innerHTML = "Incorrect!";
+                }
+            }
+        };         
+        XML.send(JSON.stringify(params));
+    }
+}
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, send_file
+from flask import Flask, render_template, send_file, request
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, origins="*")
 
 @app.route('/')
 def home():
@@ -20,11 +22,16 @@ def getQuestion():
         data = file.readlines()
         return data[int(data[0])].split(";")[0]
     
-@app.route("/compareResult")
+@app.route("/compareResult",  methods=["POST"])
 def compareResult():
     with open("data.csv", "r") as file:
         data = file.readlines()
-        return data[int(data[0])].split(";")[1] == True
+    print(request.args.get("guess"))
+    print(data[int(data[0])].split(";")[1])
+    if request.args.get("guess") == data[int(data[0])].split(";")[1]:
+        return "True"
+    else:
+        return "False"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0")
